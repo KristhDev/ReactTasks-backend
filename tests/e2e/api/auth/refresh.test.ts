6bb49e2ff4f53a6668f61982d74e2b1b8eacd532
KristhDev <kristhdev@gmail.com>
2024-02-01 02:07:57 -0600

    Test(e2e): add test in refresh endpoint

---
+++ tests/e2e/api/auth/refresh.test.ts
@@ -0,0 +1,97 @@
+/* Setup */
+import { request } from '../../../../jest.setup';
+
+/* Database */
+import { TokenRepository, UserRepository } from '../../../../src/database';
+
+/* Server */
+import { Http } from '../../../../src/server';
+
+/* Modules */
+import { JWT, JWTErrorMessages } from '../../../../src/modules/auth';
+
+describe('Test in Refresh Endpoint', () => {
+    it('should refresh token', async () => {
+        const user = await UserRepository.findOne({ email: 'tester@gmail.com' });
+        const jwt = JWT.generateToken({ id: user?.id! });
+
+        const resp = await request
+            .get('/api/auth/refresh')
+            .set('Authorization', `Bearer ${ jwt }`);
+
+        expect(resp.status).toBe(Http.OK);
+
+        expect(resp.body).toEqual({
+            status: Http.OK,
+            token: expect.any(String),
+            user: UserRepository.endpointAdapter(user!)
+        });
+
+        await Promise.all([
+            TokenRepository.deleteOne({ token: jwt }),
+            TokenRepository.deleteOne({ token: resp.body.token })
+        ]);
+    });
+
+    it('should faild because user is unauthenticated', async () => {
+        const resp = await request.get('/api/auth/refresh');
+
+        expect(resp.status).toBe(Http.UNAUTHORIZED);
+
+        expect(resp.body).toEqual({
+            msg: 'Necesita ingresar para poder realizar está acción.',
+            status: Http.UNAUTHORIZED
+        });
+    });
+
+    it('should faild because token is invalid', async () => {
+        const token = 'invalid';
+
+        const resp = await request
+            .get('/api/auth/refresh')
+            .set('Authorization', `Bearer ${ token }`);
+
+        expect(resp.status).toBe(Http.UNAUTHORIZED);
+
+        expect(resp.body).toEqual({
+            msg: 'Necesita ingresar para poder realizar está acción.',
+            status: Http.UNAUTHORIZED
+        });
+    });
+
+    it('should faild because token is revoked', async () => {
+        const user = await UserRepository.findOne({ email: 'tester@gmail.com' });
+        const jwt = JWT.generateToken({ id: user?.id! });
+
+        await JWT.revokeToken(jwt);
+
+        const resp = await request
+            .get('/api/auth/refresh')
+            .set('Authorization', `Bearer ${ jwt }`);
+
+        expect(resp.status).toBe(Http.UNAUTHORIZED);
+
+        expect(resp.body).toEqual({
+            msg: 'Necesita ingresar para poder realizar está acción.',
+            status: Http.UNAUTHORIZED
+        });
+    });
+
+    it('should faild because token is expired', async () => {
+        const user = await UserRepository.findOne({ email: 'tester@gmail.com' });
+        const jwt = JWT.generateToken({ id: user?.id! }, '1s');
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        const resp = await request
+            .get('/api/auth/refresh')
+            .set('Authorization', `Bearer ${ jwt }`);
+
+        expect(resp.status).toBe(Http.UNAUTHORIZED);
+
+        expect(resp.body).toEqual({
+            msg: JWTErrorMessages.EXPIRED,
+            status: Http.UNAUTHORIZED
+        });
+    });
+});
