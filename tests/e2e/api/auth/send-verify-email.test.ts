48d6be75b09670a772341c823dd4d91df5a99d26
KristhDev <kristhdev@gmail.com>
2024-02-06 13:43:49 -0600

    Test(e2e): add test in Send Email Verification Endpoint

---
+++ tests/e2e/api/auth/send-verify-email.test.ts
@@ -0,0 +1,115 @@
+/* Test */
+import { request } from '@test';
+
+/* Server */
+import { Http } from '@server';
+
+/* Database */
+import { Database, UserRepository } from '@database';
+
+/* Auth */
+import { AuthErrorMessages, EmailService } from '@auth';
+
+const sendEmailVerificationSpy = jest.spyOn(EmailService, 'sendEmailVerification')
+    .mockImplementation(() => Promise.resolve());
+
+describe('Test in Send Email Verification Endpoint', () => {
+    const database = new Database();
+
+    beforeAll(async () => {
+        await database.connect();
+    });
+
+    afterAll(async () => {
+        await database.disconnect();
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should send email verification', async () => {
+        await UserRepository.deleteOne({ email: 'test-e2e@gmail.com' });
+
+        const newUser = await UserRepository.create({
+            name: 'Test',
+            lastname: 'Test',
+            email: 'test-e2e@gmail.com',
+            password: 'test-test-1234',
+            verified: false
+        });
+
+        const resp = await request
+            .post('/api/auth/verify-email')
+            .send({ email: newUser.email });
+
+        expect(resp.status).toBe(200);
+
+        expect(resp.body).toEqual({
+            msg: 'Hemos enviado un correo de verificación, por favor confirma tu cuenta.', 
+            status: Http.OK
+        });
+
+        expect(sendEmailVerificationSpy).toHaveBeenCalledTimes(1);
+        expect(sendEmailVerificationSpy).toHaveBeenCalledWith({
+            email: newUser.email,
+            name: newUser.name,
+            token: expect.any(String)
+        });
+
+        await UserRepository.deleteOne({ _id: newUser._id });
+    });
+
+    it('should faild because user not found', async () => {
+        const resp = await request
+            .post('/api/auth/verify-email')
+            .send({ email: 'email-not-found@gmail.com' });
+
+        expect(resp.status).toBe(404);
+
+        expect(resp.body).toEqual({
+            msg: AuthErrorMessages.NOT_FOUND,
+            status: Http.NOT_FOUND
+        });
+
+        expect(sendEmailVerificationSpy).toHaveBeenCalledTimes(0);
+    });
+
+    it('should faild because user already verified', async () => {
+        await UserRepository.deleteOne({ email: 'test-e2e@gmail.com' });
+
+        const newUser = await UserRepository.create({
+            name: 'Test',
+            lastname: 'Test',
+            email: 'test-e2e@gmail.com',
+            password: 'test-test-1234',
+            verified: true
+        });
+
+        const resp = await request
+            .post('/api/auth/verify-email')
+            .send({ email: newUser.email });
+
+        expect(resp.status).toBe(400);
+
+        expect(resp.body).toEqual({
+            msg: 'Tu cuenta ya ha sido verificada.',
+            status: Http.BAD_REQUEST
+        });
+
+        await UserRepository.deleteOne({ _id: newUser._id });
+    });
+
+    it('should faild because email is invalid', async () => {
+        const resp = await request
+            .post('/api/auth/verify-email')
+            .send({ email: 'invalid-email' });
+
+        expect(resp.status).toBe(400);
+
+        expect(resp.body).toEqual({
+            msg: AuthErrorMessages.EMAIL_INVALID,
+            status: Http.BAD_REQUEST
+        });
+    });
+});
