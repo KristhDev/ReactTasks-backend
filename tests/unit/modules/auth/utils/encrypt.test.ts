ec8bd1fcfc5a19ba037cef8b1f9f0455d0e0e31b
KristhDev <kristhdev@gmail.com>
2024-02-08 11:35:02 -0600

    Test(unit): add test in util Encrypt of auth module

---
+++ tests/unit/modules/auth/utils/encrypt.test.ts
@@ -0,0 +1,22 @@
+import { Encrypt } from '@auth';
+
+describe('Test in util Encrypt', () => {
+    it('should create a hash from the given value', () => {
+        const value = 'test-hash';
+        const hash = Encrypt.createHash(value);
+
+        expect(hash).not.toBe(value);
+        expect(hash.length).toBeGreaterThan(value.length);
+    });
+
+    it('should compare a value with a hash and return true if they match, false otherwise', () => {
+        const value = 'test-hash';
+        const hash = Encrypt.createHash(value);
+
+        const match = Encrypt.compareHash(value, hash);
+        const noMatch = Encrypt.compareHash('wrong-value', hash);
+
+        expect(match).toBeTruthy();
+        expect(noMatch).toBeFalsy();
+    });
+});
