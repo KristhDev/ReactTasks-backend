446768de5d84f00e48203165202841788c34e3aa
KristhDev <kristhdev@gmail.com>
2024-02-10 11:02:19 -0600

    Test(unit): add test in util errors of tasks module

---
+++ tests/unit/modules/tasks/utils/errors.test.ts
@@ -0,0 +1,7 @@
+import { TaskErrorMessages } from '@tasks';
+
+describe('Test in util errors of tasks module', () => {
+    it('should to match snapshot - TaskErrorMessages', () => {
+        expect(TaskErrorMessages).toMatchSnapshot();
+    });
+});
