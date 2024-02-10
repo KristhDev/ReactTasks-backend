0bc851eb7399d02c7fac95a8782da60c62d7d4f5
KristhDev <kristhdev@gmail.com>
2024-02-10 09:59:53 -0600

    Refactor(images): add INVALID_URL message in util ImageErrorMessages

--- src/modules/images/utils/errors.ts
+++ src/modules/images/utils/errors.ts
@@ -9,6 +9,7 @@ export class ImageError extends Error {
 }
 
 export const ImageErrorMessages = {
+    INVALID_URL: 'La URL de la imagen no es valida.',
     INVALID: 'La imagen debe tener uno de los siguientes formatos: ' + Constants.ACCEPTED_IMAGE_TYPES.join(', '),
     ONE_FILE: 'Solo puedes subir una imagen.',
     REQUIRED: 'La imagen es requerida.',
