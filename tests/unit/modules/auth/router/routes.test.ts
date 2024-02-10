b92fe744ce7bff30b9c209331184198cad6626b1
KristhDev <kristhdev@gmail.com>
2024-02-09 20:55:42 -0600

    Test(unit): add test in routes of auth module

---
+++ tests/unit/modules/auth/router/routes.test.ts
@@ -0,0 +1,7 @@
+import { usersRoutes } from '@auth';
+
+describe('Test in routes of auth module', () => {
+    it('should to match snapshot', () => {
+        expect(usersRoutes).toMatchSnapshot();
+    });
+});
