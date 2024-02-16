d4785f780c6fb219ab53397009a3b661bf52648b
KristhDev <kristhdev@gmail.com>
2024-02-15 23:56:47 -0600

    Test(e2e): add test in Remove Verifications Endpoint

---
+++ tests/e2e/api/auth/remove-verifications.test.ts
@@ -0,0 +1,53 @@
+/* Database */
+import { Database } from '@database';
+import { Http } from '@server';
+import { request } from '@test';
+
+const database = new Database();
+
+describe('Test in Remove Verifications Endpoint', () => {
+    beforeAll(async () => {
+        await database.connect();
+    });
+
+    afterAll(async () => {
+        await database.disconnect();
+    });
+
+    it('should remove expired verifications of all users', async () => {
+        const resp = await request
+            .post('/api/auth/remove-verifications')
+            .set('Authorization', `Bearer ${ process.env.AUTH_SECRET! }`);
+
+        expect(resp.status).toBe(Http.OK);
+
+        expect(resp.body).toEqual({
+            msg: 'Se eliminaron todas las verificaciones expiradas correctamente.',
+            status: Http.OK
+        });
+    });
+
+    it('should faild because token isnt provided', async () => {
+        const resp = await request.post('/api/auth/remove-verifications');
+
+        expect(resp.status).toBe(Http.NOT_FOUND);
+
+        expect(resp.body).toEqual({
+            msg: 'Lo sentimos, pero no encontramos la página solicitada.',
+            status: Http.NOT_FOUND
+        });
+    });
+
+    it('should faild because token is invalid', async () => {
+        const resp = await request
+            .post('/api/auth/remove-verifications')
+            .set('Authorization', 'Bearer invalid');
+
+        expect(resp.status).toBe(Http.NOT_FOUND);
+
+        expect(resp.body).toEqual({
+            msg: 'Lo sentimos, pero no encontramos la página solicitada.',
+            status: Http.NOT_FOUND
+        });
+    });
+});
