44caf112e7311eafab5f76757aadea9df178eb67
KristhDev <kristhdev@gmail.com>
2024-02-17 16:45:40 -0600

    Test(unit): add test in util errors of server module

---
+++ tests/unit/server/utils/errors.test.ts
@@ -0,0 +1,7 @@
+import { ServerErrorMessages } from '@server';
+
+describe('Test in util errors of server module', () => {
+    it('should to match snapshot - ServerErrorMessages', () => {
+        expect(ServerErrorMessages).toMatchSnapshot();
+    });
+});
