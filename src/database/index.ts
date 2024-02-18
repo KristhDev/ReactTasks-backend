144f8ac1806238cba6ff136a9f8ca18a69dab08b
KristhDev <kristhdev@gmail.com>
2024-02-18 00:50:05 -0600

    Refactor(database): remove seeders of database module

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,5 +2,4 @@ export { default as Database } from './database';
 export * from './interfaces';
 export * from './models';
 export * from './repositories';
-export * from './seeders';
 export * from './utils';
