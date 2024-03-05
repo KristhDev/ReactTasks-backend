875e5bcf5fcad02ea1b7c43a7cdc332e67ee3b0b
KristhDev <kristhdev@gmail.com>
2024-03-04 22:31:24 -0600

    Test(unit): add test in controller StoreTaskController of tasks module

---
+++ tests/unit/modules/tasks/controllers/store.test.ts
@@ -0,0 +1,104 @@
+/* Mocks */
+import { createRequestMock, createResponseMock, imageMock, imageUrlMock, taskMock, userMock } from '@mocks';
+
+/* Server */
+import { Http, ServerErrorMessages } from '@server';
+
+/* Database */
+import { TaskRepository } from '@database';
+
+/* Modules */
+import { StoreTaskController } from '@tasks';
+import { ImageService } from '@images';
+
+const createTaskSpy = jest.spyOn(TaskRepository, 'create');
+const uploadImageSpy = jest.spyOn(ImageService, 'upload');
+
+describe('Test in StoreTaskController of tasks module', () => {
+    const { mockClear, res } = createResponseMock();
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockClear();
+    });
+
+    it('should has handler method', () => {
+        expect(StoreTaskController).toHaveProperty('handler');
+    });
+
+    it('should return a task', async () => {
+        taskMock.image = imageUrlMock;
+        createTaskSpy.mockResolvedValue(taskMock);
+        uploadImageSpy.mockResolvedValue(imageUrlMock);
+
+        const req = createRequestMock({
+            auth: {
+                user: userMock
+            },
+            body: {
+                title: taskMock.title,
+                description: taskMock.description,
+                deadline: taskMock.deadline
+            },
+            files: {
+                image: imageMock
+            }
+        });
+
+        await StoreTaskController.handler(req, res);
+
+        expect(createTaskSpy).toHaveBeenCalledTimes(1);
+        expect(createTaskSpy).toHaveBeenCalledWith({
+            title: taskMock.title,
+            description: taskMock.description,
+            deadline: taskMock.deadline,
+            userId: userMock._id,
+            image: imageUrlMock
+        });
+
+        expect(res.status).toHaveBeenCalledTimes(1);
+        expect(res.status).toHaveBeenCalledWith(Http.CREATED);
+
+        expect(res.json).toHaveBeenCalledTimes(1);
+        expect(res.json).toHaveBeenCalledWith({
+            msg: 'Haz agregado la tarea correctamente.',
+            status: Http.CREATED,
+            task: TaskRepository.endpointAdapter(taskMock)
+        });
+    });
+
+    it('should return internal server error', async () => {
+        createTaskSpy.mockRejectedValue(new Error('Database error'));
+
+        const req = createRequestMock({
+            auth: {
+                user: userMock
+            },
+            body: {
+                title: taskMock.title,
+                description: taskMock.description,
+                deadline: taskMock.deadline
+            }
+        });
+
+        await StoreTaskController.handler(req, res);
+
+        expect(createTaskSpy).toHaveBeenCalledTimes(1);
+        expect(createTaskSpy).toHaveBeenCalledWith({
+            title: taskMock.title,
+            description: taskMock.description,
+            deadline: taskMock.deadline,
+            userId: userMock._id,
+            image: ''
+        });
+
+        expect(res.status).toHaveBeenCalledTimes(1);
+        expect(res.status).toHaveBeenCalledWith(Http.INTERNAL_SERVER_ERROR);
+
+        expect(res.json).toHaveBeenCalledTimes(1);
+        expect(res.json).toHaveBeenCalledWith({
+            msg: ServerErrorMessages.INTERNAL_SERVER_ERROR,
+            status: Http.INTERNAL_SERVER_ERROR
+        });
+    });
+});
