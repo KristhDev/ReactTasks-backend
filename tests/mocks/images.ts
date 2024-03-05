1f24b98bc38fb562df8dc041e88c8a887ddc88d2
KristhDev <kristhdev@gmail.com>
2024-03-05 07:52:24 -0600

    Test(mocks): add new mock in images

--- tests/mocks/images.ts
+++ tests/mocks/images.ts
@@ -1,6 +1,7 @@
 import { UploadedFile } from 'express-fileupload';
 
 export const imageUrlMock = 'https://res.cloudinary.com/dzs8lf9lc/image/upload/v1702530226/react-tasks/tasks/erkntrgjdi15gxmxspdu.jpg';
+export const newImageUrlMock = 'https://res.cloudinary.com/dzs8lf9lc/image/upload/v1702530226/react-tasks/tasks/sfkqjvqg9qj9v3v4vq2.jpg';
 
 export const imageMock: UploadedFile = {
     data: Buffer.from(''),
